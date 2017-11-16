7c111445894bd7ba4257bb416c3dca0d98b30479
corentin <corentinmistere@gmail.com>
2017-11-16 16:15:36 +0100

    Exo 4 5 6

--- array/app.js
+++ array/app.js
@@ -13,3 +13,20 @@ console.log(array)
 /**exo 3 début**/
 var array= ["Juin"];
 console.log(array)
+/**exo 3 fin**/
+
+/**exercice 4 début**/
+var array= ["Novembre"];
+console.log(array)
+/**exo 4 fin**/
+
+/**exo 5 début**/
+var array= ["Août"];
+console.log(array)
+/**exo 5 fin, rectificationen ajoutant la <meta> non incluse au début**/
+
+/**exo 6 début**/
+var array = ["Janvier", "Fevrier", "Mars", "Avril", "Mais", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"];
+console.log(array)
+/**exo 6 fin**/
+
