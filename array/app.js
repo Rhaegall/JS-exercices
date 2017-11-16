33c7c5c791c170a8dbc23da80001a1a1c3ba4bc9
corentin <corentinmistere@gmail.com>
2017-11-16 15:48:59 +0100

    Exercice 2 et 3.

--- array/app.js
+++ array/app.js
@@ -1,9 +1,15 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 
 /**exo 1 début**/
-var array = ["Janvier", "Fevrier", "Mars", "Avril", "Mais", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"];
-
+var array = ["Janvier", "Fevrier", "Mars", "Avril", "Mais", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"];
 console.log(array)
 /**exo 1 fin**/
 
 /**exo 2 début**/
+var array= ["Mars"];
+console.log(array)
+/**exo 2 fin**/
+
+/**exo 3 début**/
+var array= ["Juin"];
+console.log(array)
