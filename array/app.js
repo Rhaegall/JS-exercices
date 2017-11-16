3eda9af4a7a16987bccd2a50c374f819ae5f71d1
corentin <corentinmistere@gmail.com>
2017-11-16 15:15:59 +0100

    Exercice 1.

--- array/app.js
+++ array/app.js
@@ -1,1 +1,9 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+
+/**exo 1 début**/
+var array = ["Janvier", "Fevrier", "Mars", "Avril", "Mais", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"];
+
+console.log(array)
+/**exo 1 fin**/
+
+/**exo 2 début**/
